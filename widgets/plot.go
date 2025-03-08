7eaa0d4dfe379fb7d07ded5a2cccaa2380565128
Stefano Scafiti <stefano.scafiti96@gmail.com>
2025-03-08 20:59:56 +0100

    Render custom labels in plot widget

--- widgets/plot.go
+++ widgets/plot.go
@@ -7,6 +7,7 @@ package widgets
 import (
 	"fmt"
 	"image"
+	"math"
 
 	. "github.com/gizak/termui/v3"
 )
@@ -95,10 +96,12 @@ func (self *Plot) renderBraille(buf *Buffer, drawArea image.Rectangle, maxVal fl
 			}
 		}
 	case LineChart:
+		r := self.GetValueRange()
+
 		for i, line := range self.Data {
-			previousHeight := int((line[1] / maxVal) * float64(drawArea.Dy()-1))
+			previousHeight := int(r.Scale(line[0]) * float64(drawArea.Dy()-1))
 			for j, val := range line[1:] {
-				height := int((val / maxVal) * float64(drawArea.Dy()-1))
+				height := int(r.Scale(val) * float64(drawArea.Dy()-1))
 				canvas.SetLine(
 					image.Pt(
 						(drawArea.Min.X+(j*self.HorizontalScale))*2,
@@ -174,26 +177,56 @@ func (self *Plot) plotAxes(buf *Buffer, maxVal float64) {
 		NewStyle(ColorWhite),
 		image.Pt(self.Inner.Min.X+yAxisLabelsWidth, self.Inner.Max.Y-1),
 	)
-	// draw rest
-	for x := self.Inner.Min.X + yAxisLabelsWidth + (xAxisLabelsGap)*self.HorizontalScale + 1; x < self.Inner.Max.X-1; {
-		label := fmt.Sprintf(
-			"%d",
-			(x-(self.Inner.Min.X+yAxisLabelsWidth)-1)/(self.HorizontalScale)+1,
-		)
-		buf.SetString(
-			label,
-			NewStyle(ColorWhite),
-			image.Pt(x, self.Inner.Max.Y-1),
-		)
-		x += (len(label) + xAxisLabelsGap) * self.HorizontalScale
+
+	if len(self.DataLabels) == 0 {
+		// draw rest
+		for x := self.Inner.Min.X + yAxisLabelsWidth + (xAxisLabelsGap)*self.HorizontalScale + 1; x < self.Inner.Max.X-1; {
+			label := fmt.Sprintf(
+				"%d",
+				(x-(self.Inner.Min.X+yAxisLabelsWidth)-1)/(self.HorizontalScale)+1,
+			)
+
+			buf.SetString(
+				label,
+				NewStyle(ColorWhite),
+				image.Pt(x, self.Inner.Max.Y-1),
+			)
+			x += (len(label) + xAxisLabelsGap) * self.HorizontalScale
+		}
+	} else {
+		xAxisLabelsGap := ((self.Inner.Max.X - self.Inner.Min.X) - yAxisLabelsWidth) / (len(self.DataLabels) - 1)
+
+		x := self.Inner.Min.X + yAxisLabelsWidth
+		for _, label := range self.DataLabels {
+			buf.SetString(
+				label,
+				NewStyle(ColorWhite),
+				image.Pt(x, self.Inner.Max.Y-1),
+			)
+
+			labelLen := len(label)
+
+			x += xAxisLabelsGap
+			if x+labelLen >= self.Inner.Max.X {
+				x = self.Inner.Max.X - labelLen
+			}
+		}
 	}
+
+	yTicks := self.yTicks()
 	// draw y axis labels
-	verticalScale := maxVal / float64(self.Inner.Dy()-xAxisLabelsHeight-1)
-	for i := 0; i*(yAxisLabelsGap+1) < self.Inner.Dy()-1; i++ {
+	//verticalScale := maxVal / float64(self.Inner.Dy()-xAxisLabelsHeight-1)
+	_yAxisLabelsGap := (self.Inner.Dy() - xAxisLabelsHeight) / (len(yTicks) - 1)
+	for i := range yTicks {
+		height := self.Inner.Max.Y - (i * (_yAxisLabelsGap)) - 2
+		if height <= 0 {
+			height = 1
+		}
+
 		buf.SetString(
-			fmt.Sprintf("%.2f", float64(i)*verticalScale*(yAxisLabelsGap+1)),
+			fmt.Sprintf("%.2f", yTicks[i]), //float64(i)*verticalScale*(yAxisLabelsGap+1)),
 			NewStyle(ColorWhite),
-			image.Pt(self.Inner.Min.X, self.Inner.Max.Y-(i*(yAxisLabelsGap+1))-2),
+			image.Pt(self.Inner.Min.X, height),
 		)
 	}
 }
@@ -224,4 +257,66 @@ func (self *Plot) Draw(buf *Buffer) {
 	case MarkerDot:
 		self.renderDot(buf, drawArea, maxVal)
 	}
+}
+
+func (self *Plot) yAxisLabelWidth() int {
+	return 0
+}
+
+const MaxYTicks = 10 // TODO: make this configurable
+
+func (self *Plot) yTicks() []float64 {
+	r := self.GetValueRange()
+	if r.IsConstant() {
+		return []float64{
+			math.Inf(1),
+			r.Min,
+			math.Inf(-1),
+		}
+	}
+
+	ticks := make([]float64, MaxYTicks)
+	gap := (r.Max - r.Min) / float64(len(ticks)-1)
+	for i := range ticks {
+		ticks[i] = r.Min + float64(i)*gap
+	}
+	return ticks
+}
+
+func (self *Plot) GetValueRange() ValueRange {
+	if len(self.Data) == 0 {
+		return ValueRange{}
+	}
+
+	max, min := self.Data[0][0], self.Data[0][0]
+	for _, slice := range self.Data {
+		for _, val := range slice {
+			if val > max {
+				max = val
+			} else if val < min {
+				min = val
+			}
+		}
+	}
+
+	return ValueRange{
+		Min: min,
+		Max: max,
+	}
+}
+
+type ValueRange struct {
+	Min float64
+	Max float64
+}
+
+func (r *ValueRange) Scale(x float64) float64 {
+	if r.IsConstant() {
+		return 0.5
+	}
+	return (x - r.Min) / (r.Max - r.Min)
+}
+
+func (r *ValueRange) IsConstant() bool {
+	return r.Max == r.Min
 }
